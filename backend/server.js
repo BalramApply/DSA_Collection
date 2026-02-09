6b50f57fb0bea604e2d45c931896a2fb2bcf9703
BalramApply <balramapply@gmail.com>
2026-02-09 16:14:55 +0530

    api cors

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ seedAdmin();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL,
+  // origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true,
 }));
 app.use(express.json());
